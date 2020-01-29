7b9a411272a987b492bd177072c933f122c77938
Marek Grochowski <marekgrochowski@web.de>
2020-01-29 19:58:46 +0000

    Namespace changed

---
+++ LazyDataReader.cs
@@ -0,0 +1,57 @@
+﻿using LazyDataReader.Models;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace LazyDataReader
+{
+    public static class Reader<T>
+        where T : class
+    {
+        #region Public Methods
+
+        public static T GetData(string path)
+        {
+            var data = ReadData(path);
+
+            return data;
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private static T ReadData(string path)
+        {
+            var result = default(T);
+
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"The file '{path}' does not exist.");
+
+            try
+            {
+                var serializer = new XmlSerializer(
+                    type: typeof(T),
+                    defaultNamespace: string.Empty);
+
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    using (var namespaceIgnorantReader = new NamespaceIgnorantReader(reader))
+                    {
+                        result = serializer.Deserialize(namespaceIgnorantReader) as T;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(
+                    message: $"The file '{path}' cannot be red.",
+                    innerException: ex);
+            }
+
+            return result;
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ Models/NamespaceIgnorantReader.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace LazyDataReader.Models
+{
+    // Helper class to ignore namespaces when de-serializing
+    internal class NamespaceIgnorantReader : XmlTextReader
+    {
+        #region Public Constructors
+
+        public NamespaceIgnorantReader(TextReader reader) : base(reader)
+        { }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public override string NamespaceURI => string.Empty;
+
+        #endregion Public Properties
+    }
+}
